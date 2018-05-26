e07e7df1ee6c4a968921bc6c9a626aa49fecc07d
Andrew Aubury <andrewaubury1@gmail.com>
2018-05-26 11:34:26 +0100

    More building stuff

--- src/main/java/org/minenite/plugin/core/objects/Building.java
+++ src/main/java/org/minenite/plugin/core/objects/Building.java
@@ -1,5 +1,9 @@
 package org.minenite.plugin.core.objects;
 
+import com.boydti.fawe.FaweAPI;
+import com.boydti.fawe.object.FaweQueue;
+import com.boydti.fawe.object.RunnableVal;
+import com.boydti.fawe.util.TaskManager;
 import lombok.Data;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -9,6 +13,7 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.util.Vector;
 import org.minenite.plugin.MineNite;
 
@@ -27,6 +32,11 @@ public class Building {
     HashMap<Vector,Material> building = new HashMap<>();
 
     public void build(Location startLoc){
+        FaweQueue queue = FaweAPI.createQueue(startLoc.getWorld().toString(), true);
+TaskManager.IMP.runUnsafe(queue,new Runnable() {
+    @Override
+    public void run() {
+
         for(Vector v : building.keySet()){
             Location startLoc2 = startLoc.clone();
             startLoc2 = startLoc2.add(new Location(startLoc.getWorld(),v.getBlockX(),v.getBlockY(),v.getBlockZ()));
@@ -34,18 +44,16 @@ public class Building {
                 Location finalStartLoc = startLoc2;
                 finalStartLoc.getBlock().setType(Material.BARRIER);
                 ThreadLocalRandom r = ThreadLocalRandom.current();
-                // use floobits chat
                 int low = 1;
                 int high = 60;
                 int result = r.nextInt(high-low) + low;
                 MineNite mineNite = MineNite.getPlugin(MineNite.class);
-                mineNite.getScheduler().runTaskLater(mineNite, new Runnable() {
-                    @Override
-                    public void run() {
-                        finalStartLoc.getBlock().setType(building.get(v));
-                    }
-                },result);
+                mineNite.getScheduler().runTaskLater(mineNite, () -> finalStartLoc.getBlock().setType(building.get(v)),result);
             }
         }
+
+    }
+});
+
     }
 }
