04ba09e39b2aa03ca50c842782f487b360bf75ee
Andrew Aubury <andrewaubury1@gmail.com>
2018-05-31 10:58:32 +0100

    Added MinePoS Maven, and Vehicles API

--- src/main/java/org/minenite/plugin/commands/Test.java
+++ src/main/java/org/minenite/plugin/commands/Test.java
@@ -1,9 +1,19 @@
 package org.minenite.plugin.commands;
 
 import com.google.inject.Inject;
+import es.pollitoyeye.vehicles.VehiclesMain;
+import es.pollitoyeye.vehicles.enums.VehicleType;
+import es.pollitoyeye.vehicles.interfaces.VehicleSubType;
+import es.pollitoyeye.vehicles.vehicle.Parachute;
+import es.pollitoyeye.vehicles.vehiclemanagers.ParachuteManager;
+import es.pollitoyeye.vehicles.vehicletypes.ParachuteType;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.minenite.plugin.core.framework.Command;
 import org.minenite.plugin.core.objects.enums.CommandsEnum;
 import org.minenite.plugin.core.storage.yaml.MFile;
@@ -25,12 +35,12 @@ public final class Test extends Command {
     @Override
     protected boolean execute(CommandSender sender, String[] args) {
         if (sender instanceof Player) {
-            Player player = (Player) sender;
-            xpUtils.addLevel(player, 5);
-            xpUtils.setPercentage(player, 0.5f);
+            Player p = (Player) sender;
 
-            FileConfiguration config = mFile.getFileConfiguration("config");
-            player.sendMessage(config.getString("test"));
+            VehicleType type = (VehicleType) VehiclesMain.getPlugin().vehicleSubTypesMap.keySet().toArray()[5];
+            ParachuteType subType = new ParachuteType(new ItemStack(Material.LAPIS_BLOCK), new ItemStack(Material.COAL_BLOCK), 10.00, p.getName()+"-PARACHUTE", p.getName()+"-PARACHUTE", 5.00, 100.00, "");
+            ArmorStand tmp = (VehicleType.PARACHUTE).getVehicleManager().spawn(p.getLocation(), (p).getUniqueId().toString(), (subType).getName());
+            tmp.setPassenger(p);
         }
 
         return false;
